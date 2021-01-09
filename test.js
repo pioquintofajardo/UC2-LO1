358b89f30984be4e82920064b1bc9f7a33b834ad
Pioquinto Fajardo <76145226+pioquintofajardo@users.noreply.github.com>
2021-01-09 09:51:10 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+// the first alert
+alert("Hello, World!");
+
+// the second alert
+alert("Hello, World!");
+
+//move the image from top tpo bottom
+//make the image invinsible
+//make the image visible
